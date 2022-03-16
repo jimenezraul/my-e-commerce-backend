4f3473bcde8a8e1815cebcbea09d8ebb89c2aa37
Raul <jimenezraul1981@gmail.com>
2022-03-16 11:20:54 -0400

    fix missing curly brackets on product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,54 +1,55 @@
-const router = require('express').Router();
-const { Product, Category, Tag, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Product, Category, Tag, ProductTag } = require("../../models");
 
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // find all products
   Product.findAll({
-    attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
-  // be sure to include its associated Category and Tag data
+    attributes: ["id", "product_name", "price", "stock", "category_id"],
+    // be sure to include its associated Category and Tag data
     include: [
       {
         model: Category,
-        as: 'category',
-        attributes: ['id', 'category_name'],
+        as: "category",
+        attributes: ["id", "category_name"],
       },
       {
         model: Tag,
-        as: 'tags',
-        attributes: ['id', 'tag_name'],
+        as: "tags",
+        attributes: ["id", "tag_name"],
       },
     ],
-  })
+  });
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find a single product by its `id`
   Product.findOne({
     where: {
       id: req.params.id,
     },
-    attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
-  // be sure to include its associated Category and Tag data
+    attributes: ["id", "product_name", "price", "stock", "category_id"],
+    // be sure to include its associated Category and Tag data
     include: [
       {
         model: Category,
-        as: 'category',
-        attributes: ['id', 'category_name'],
+        as: "category",
+        attributes: ["id", "category_name"],
       },
       {
         model: Tag,
-        as: 'tags',
-        attributes: ['id', 'tag_name'],
+        as: "tags",
+        attributes: ["id", "tag_name"],
       },
     ],
+  });
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -80,7 +81,7 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
@@ -121,8 +122,13 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  });
 });
 
 module.exports = router;
