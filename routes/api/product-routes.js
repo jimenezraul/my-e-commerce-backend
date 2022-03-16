78ddedc204e0f6983f9b94142ef06ada5050e649
Raul <jimenezraul1981@gmail.com>
2022-03-16 11:17:13 -0400

    add api for product and tag

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,13 +6,45 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
+  Product.findAll({
+    attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
   // be sure to include its associated Category and Tag data
+    include: [
+      {
+        model: Category,
+        as: 'category',
+        attributes: ['id', 'category_name'],
+      },
+      {
+        model: Tag,
+        as: 'tags',
+        attributes: ['id', 'tag_name'],
+      },
+    ],
+  })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
+  Product.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
   // be sure to include its associated Category and Tag data
+    include: [
+      {
+        model: Category,
+        as: 'category',
+        attributes: ['id', 'category_name'],
+      },
+      {
+        model: Tag,
+        as: 'tags',
+        attributes: ['id', 'tag_name'],
+      },
+    ],
 });
 
 // create new product
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,28 +1,74 @@
-const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
+  Tag.findAll({
+    attributes: ["id", "tag_name"],
+    // be sure to include its associated Product data
+    include: [
+      {
+        model: Product,
+        as: "products",
+        attributes: ["id", "product_name"],
+      },
+    ],
+  });
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "tag_name"],
+    // be sure to include its associated Product data
+    include: [
+      {
+        model: Product,
+        as: "products",
+        attributes: ["id", "product_name"],
+      },
+    ],
+  });
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+    .then((tag) => {
+      res.json(tag);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((tag) => {
+      res.json(tag);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
 });
 
 module.exports = router;
